56aee91c78ccd2ea2890ac871d78515142d639a7
Omid Mirzaei <omidomirzaei@gmail.com>
2026-09-19 10:21:05 +0400

    add Carbon arrows for the event path hops

---
+++ web/components/icons/arrow-down.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from "react";
+
+export function ArrowDownIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
+      <path
+        fill="currentColor"
+        d="M24.59 16.59L17 24.17V4h-2v20.17l-7.59-7.58L6 18l10 10l10-10z"
+      />
+    </svg>
+  );
+}
---
+++ web/components/icons/arrow-right.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from "react";
+
+export function ArrowRightIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
+      <path
+        fill="currentColor"
+        d="m18 6l-1.43 1.393L24.15 15H4v2h20.15l-7.58 7.573L18 26l10-10z"
+      />
+    </svg>
+  );
+}
